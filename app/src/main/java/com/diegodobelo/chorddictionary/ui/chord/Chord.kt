be97e3c38fac83977fd61bc8f9159f258af57809
Diego Bezerra <diego.bezerra@gmail.com>
2023-05-02 16:11:10 -0300

    added Note and String markers composables

--- app/src/main/java/com/diegodobelo/chorddictionary/ui/chord/Chord.kt
+++ app/src/main/java/com/diegodobelo/chorddictionary/ui/chord/Chord.kt
@@ -11,14 +11,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 private fun getVerticalGradientWithTransparentEnds(
     color: Color
@@ -33,10 +42,68 @@ private fun getVerticalGradientWithTransparentEnds(
 }
 
 @Composable
-fun Dot() {
-    Canvas(modifier = Modifier.size(10.dp), onDraw = {
-        drawCircle(color = Color.Black)
+fun Note(
+    fingerNumber: Int
+) {
+    Box {
+        Canvas(modifier = Modifier.size(14.dp), onDraw = {
+            drawCircle(color = Color.Black)
+        })
+        Text(
+            text = fingerNumber.toString(),
+            modifier = Modifier
+                .align(Alignment.Center),
+            color = Color.White,
+            fontSize = 8.sp,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+private fun PlayableStringMarker(
+    isFill: Boolean
+) {
+    Canvas(modifier = Modifier.size(8.dp), onDraw = {
+        drawCircle(
+            color = Color.Black,
+            style = if (isFill) Fill else Stroke(width = 1.dp.toPx())
+        )
     })
+}
+
+@Composable
+private fun UnplayableStringMarker() {
+    Canvas(
+        modifier = Modifier
+            .width(8.dp)
+            .height(8.dp)
+    ) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+        drawLine(
+            color = Color.Black,
+            start = Offset(x = canvasWidth, y = canvasHeight),
+            end = Offset(x = 0f, y = 0f),
+            strokeWidth = 1.dp.toPx()
+        )
+        drawLine(
+            color = Color.Black,
+            start = Offset(x = canvasWidth, y = 0f),
+            end = Offset(x = 0f, y = canvasHeight),
+            strokeWidth = 1.dp.toPx()
+        )
+    }
+}
+
+@Composable
+fun BassStringMarker() {
+    PlayableStringMarker(isFill = true)
+}
+
+@Composable
+fun NormalStringMarker() {
+    PlayableStringMarker(isFill = false)
 }
 
 @Composable
@@ -169,5 +236,5 @@ fun GuitarArm() {
 @Preview(showBackground = true)
 @Composable
 fun ChordPreview() {
-    GuitarArm()
+    Note(2)
 }
